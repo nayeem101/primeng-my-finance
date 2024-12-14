77a177e1e325ecad512c9e4bcb21949b838e4cd7
nayeem101 <inayeem044@gmail.com>
2024-12-14 23:02:21 +0600

    bumped ngrx-store to v18.1.1 as v19 not stable

--- src/app/shared/store/transactions.store.ts
+++ src/app/shared/store/transactions.store.ts
@@ -1,18 +1,24 @@
 import { patchState, signalStore, withHooks, withMethods } from '@ngrx/signals';
-import { addEntities, addEntity, withEntities } from '@ngrx/signals/entities';
+import {
+  addEntities,
+  addEntity,
+  removeEntity,
+  withEntities,
+} from '@ngrx/signals/entities';
 import { user1 } from '../models/user.data';
-import { AccountTransaction, Transaction } from '../models/user.model';
+import { AccountTransaction } from '../models/user.model';
 
 export const TransactionStore = signalStore(
   withEntities<AccountTransaction>(),
-  withMethods((store) => {
-    return {
-      addTransaction(entity: AccountTransaction) {
-        console.log(entity);
-        patchState(store, addEntity(entity));
-      },
-    };
-  }),
+  withMethods((store) => ({
+    addTransaction(entity: AccountTransaction) {
+      console.log(entity);
+      patchState(store, addEntity(entity));
+    },
+    removeTransaction(id: number) {
+      patchState(store, removeEntity(id));
+    },
+  })),
   withHooks({
     onInit(store) {
       //initial transactions
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -23,17 +23,17 @@ import { TransactionFormComponent } from './components/transaction-form/transact
 import { TransactionStore } from '../shared/store/transactions.store';
 
 @Component({
-    selector: 'app-transactions',
-    imports: [TableModule, ButtonModule, ConfirmDialogModule, ToastModule],
-    templateUrl: './transactions.component.html',
-    styleUrl: './transactions.component.css',
-    providers: [
-        ConfirmationService,
-        MessageService,
-        DialogService,
-        TransactionStore,
-    ],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-transactions',
+  imports: [TableModule, ButtonModule, ConfirmDialogModule, ToastModule],
+  templateUrl: './transactions.component.html',
+  styleUrl: './transactions.component.css',
+  providers: [
+    ConfirmationService,
+    MessageService,
+    DialogService,
+    TransactionStore,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TransactionsComponent implements OnDestroy {
   private confirmationService = inject(ConfirmationService);
@@ -84,10 +84,13 @@ export class TransactionsComponent implements OnDestroy {
       rejectIcon: 'none',
 
       accept: () => {
+        // delete transaction
+        this.transactionsStore.removeTransaction(transaction.id);
+        // show message
         this.messageService.add({
           severity: 'info',
           summary: 'Confirmed',
-          detail: 'Record deleted',
+          detail: 'Transaction deleted',
         });
       },
       reject: () => {
